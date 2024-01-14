ed2560257efc7e0820afd56e17fc1d3576d357cd
“Bibek <bibekpoudel442@gmail.com>
2024-01-14 20:00:35 +0545

    ABC_336_A

---
+++ LongLoong.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    cout << "L";
+    while (x--)
+        cout << "o";
+    cout << "ng" << endl;
+    return 0;
+}
